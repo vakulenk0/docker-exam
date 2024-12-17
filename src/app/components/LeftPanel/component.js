3d4994d0661733bdd1d99c1d42f74bd594a34b2a
TIMA <timofey.fomin@gmail.com>
2024-12-17 03:41:38 +0300

    commit

--- src/app/components/LeftPanel/component.js
+++ src/app/components/LeftPanel/component.js
@@ -2,21 +2,23 @@
 
 import Image from 'next/image';
 
-const LeftPanel = ({ onToggleDrawing, onAddText }) => {
+const LeftPanel = ({ onToggleDrawing, onAddText, activeMode, onAddArrow }) => {
     return (
         <div
             className="absolute left-4 top-1/2 transform -translate-y-1/2 bg-gray-800 p-2 flex flex-col items-center space-y-2.5 z-10 rounded-lg shadow-lg">
             <div
-                className="w-12 h-12 bg-gray-700 hover:bg-gray-600 rounded flex items-center justify-center cursor-pointer"
+                className={`w-12 h-12 hover:bg-gray-600 rounded flex items-center justify-center cursor-pointer ${activeMode === 'drawing' ? 'bg-gray-500' : 'bg-gray-700'}`}
                 onClick={onToggleDrawing}
             >
                 <Image src="/panelicons/cursor.svg" alt="Cursor" width={32} height={32}/>
             </div>
-            <div className="w-12 h-12 bg-gray-700 hover:bg-gray-600 rounded flex items-center justify-center">
+            <div
+                className="w-12 h-12 bg-gray-700 hover:bg-gray-600 rounded flex items-center justify-center cursor-pointer"
+            >
                 <Image src="/panelicons/figure.svg" alt="Figure" width={32} height={32}/>
             </div>
             <div
-                className="w-12 h-12 bg-gray-700 hover:bg-gray-600 rounded flex items-center justify-center cursor-pointer"
+                className={`w-12 h-12 hover:bg-gray-600 rounded flex items-center justify-center cursor-pointer ${activeMode === 'textAdding' ? 'bg-gray-500' : 'bg-gray-700'}`}
                 onClick={onAddText}
             >
                 <Image src="/panelicons/text.svg" alt="Text" width={32} height={32}/>
